589db01c2bbda6bd556cdafa5fdc3d041eb2b16d
hp <hp@DESKTOP-9U2QFOR>
2021-04-12 19:36:54 +0530

    First Commit

---
+++ src/main/java/gitPractice/LearnWebDriverManager.java
@@ -0,0 +1,37 @@
+package gitPractice;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import com.gargoylesoftware.htmlunit.javascript.background.JavaScriptExecutor;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LearnWebDriverManager {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		 WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		 driver.get("https://www.google.com/"); 
+		 System.out.println(driver.getTitle());
+		
+		driver.close();
+	
+		  
+		
+		
+		
+		
+
+	}
+
+}
