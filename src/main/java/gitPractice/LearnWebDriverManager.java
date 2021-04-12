38318bbb6caa4736f5b7ded4577a5259c48f4b8f
hp <hp@DESKTOP-9U2QFOR>
2021-04-12 19:59:02 +0530

    Third commit on Demo Jenkin Run

--- src/main/java/gitPractice/LearnWebDriverManager.java
+++ src/main/java/gitPractice/LearnWebDriverManager.java
@@ -1,35 +1,11 @@
 package gitPractice;
-import java.util.Set;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WindowType;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
-
-import com.gargoylesoftware.htmlunit.javascript.background.JavaScriptExecutor;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LearnWebDriverManager {
 
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
-		 WebDriverManager.chromedriver().setup();
-		ChromeDriver driver = new ChromeDriver();
-		 driver.get("https://www.google.com/"); 
-		 System.out.println(driver.getTitle());
-		System.out.println("CC");
-		driver.close();
-	
-		  
 		
-		
-		
+		System.out.println("Demo Run for Jenkin Run...");
+		System.err.println("Demo Jenkin Run...");
 		
 
 	}
